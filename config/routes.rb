789944ed148c8eaa96aed30094a2f1aca18e97ea
Greg Vissing <greg.vissing@gmail.com>
2017-03-10 22:39:51 +0000

    Added about view, route, controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root to: 'pages#home'           # route for Home page
+  get 'about', to: 'pages#about'  # route for About page
 end
