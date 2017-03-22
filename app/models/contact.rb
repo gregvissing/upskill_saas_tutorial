0ab8d5117b701ad4d2180d71b4081290bb62a0f8
Greg Vissing <greg.vissing@gmail.com>
2017-03-22 02:59:14 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
-    
+    # validate form
+    # will save if each is true
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :comments, presence: true
 end
