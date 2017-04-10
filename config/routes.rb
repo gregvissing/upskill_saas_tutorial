e02e0a7e57294f5cfda1e759099e1168c5f73365
Greg Vissing <greg.vissing@gmail.com>
2017-04-10 02:48:48 +0000

    added conditional log out button

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'pages#home'                           # route for Home page
     get 'about', to: 'pages#about'                  # route for About page
