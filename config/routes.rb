c80104cf92f3ba726a9da2658a8399c2148f7214
Greg Vissing <greg.vissing@gmail.com>
2017-04-26 02:47:49 +0000

    Added code comments for Stripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-    devise_for :users
-    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root to: 'pages#home'                           # route for Home page
+    devise_for :users, controllers: { registrations: 'users/registrations' }
     get 'about', to: 'pages#about'                  # route for About page
     resources :contacts, only: :create      # generatoes create paths for Contacts; resources :contacts => would create all paths for Contacts
-    
     get 'contact-us', to: 'contacts#new', as: 'new_contact'  # custom url for Contact Us page; contacts#new -> contacts Controller, new Action
 end
